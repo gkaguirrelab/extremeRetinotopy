c2591c646a72da4e9c135e761a5592ea4bd829f7
Adel Qalieh <aqalieh95@gmail.com>
2017-05-01 14:54:48 -0400

    [ENH] Add tests for angle transformation

---
+++ tests.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import pytest
+import numpy as np
+
+
+def transform_rh_polar(rh_pol_data):
+    """Transforms polar angles from the right hemisphere to be centered around
+    0, going from -π to +π"""
+    return np.pi - rh_pol_data % (2 * np.pi)
+
+test_angles = [
+    (0, np.pi),
+    (np.pi, 0),
+    (np.pi - 1, 1),
+    (1 - np.pi, -1),
+]
+
+
+@pytest.mark.parametrize("angle,expected", test_angles)
+def test_transform_angle(angle, expected):
+    assert transform_rh_polar(angle) == expected
