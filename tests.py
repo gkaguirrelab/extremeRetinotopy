c8a83117a24efdfa0dc9851b6181148ecf2b26fd
Adel Qalieh <aqalieh95@gmail.com>
2017-05-01 14:59:24 -0400

    [DOC] Add extra documentation on method and +pi bias

--- tests.py
+++ tests.py
@@ -5,12 +5,14 @@ import numpy as np
 
 def transform_rh_polar(rh_pol_data):
     """Transforms polar angles from the right hemisphere to be centered around
-    0, going from -π to +π"""
+    0, going from -π to +π. The angles are mirrored horizontally, such that 0 ↔
+    ±π, although this has a bias towards +π"""
     return np.pi - rh_pol_data % (2 * np.pi)
 
 test_angles = [
     (0, np.pi),
     (np.pi, 0),
+    (-np.pi, 0),
     (np.pi - 1, 1),
     (1 - np.pi, -1),
     (np.pi / 2, np.pi / 2),
